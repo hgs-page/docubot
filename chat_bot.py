ad6666006c00aeb1c68fa348b21008c5563ff4e3
limgeunil <rmsdlf201022149@gmail.com>
2024-04-18 11:50:33 +0900

    update chatbot def

--- chat_bot.py
+++ chat_bot.py
@@ -11,8 +11,13 @@ class ChatBot:
 		self.model = ChatGoogleGenerativeAI(model=model, temperature=temperature)
 
 	def create_chain(self, prompt):
-		# code
-		pass
+		vector_db = self.document_processor.create_vector_db()
+		retriever = self.get_retriever(vector_db)
+		return RunnableMap({
+			"context": lambda x: retriever.get_relevant_documents(x['question']),
+			"question": lambda x: x['question']
+		}) | prompt | self.model
+		
 
 	def get_retriever(self, docsearch):
 		# code
