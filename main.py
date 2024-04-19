a887a815d0ef5effe35228e1902a27d5fc1f7cc2
hgs <t00561526@gmail.com>
2024-04-19 06:19:48 +0000

    update main.py logger 적용

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
+from logger import Logger
 from IPython.display import Markdown
 from chat_bot import ChatBot
 from prompt_creator import PromptCreator
 from config import API_KEY
 from config import PDF_DOCUMENT
 
+class Main:
+	def __init__(self, api_key:str, pdf_document:str) -> None:
+		self.logger = Logger("history.json")
+		self.chat_bot = ChatBot(api_key, pdf_document)
+		self.prompt_creator = PromptCreator()
+		self.template = self.prompt_creator.create_template()
+		self.prompt = self.prompt_creator.from_template(self.template)
+		self.chain = self.chat_bot.create_chain(self.prompt)
+		pass
+	def run(self):
+		self.logger.load()
+		while True:
+			question = input("질문을 입력하세요(기록 보기: 1, 기록삭제: 2  종료: exit): ")
+			if len(question) == 0:
+				print("질문을 입력하세요.")
+				continue
+			if question.lower() == 'exit':
+				print("대화를 종료합니다")
+				return
+			elif question == "1":
+				self.logger.print()
+			elif question == "2":
+				self.logger.clear()
+			else:
+				try:
+					print("처리중...\n")
+					answer = self.chain.invoke({'question': question}).content
+					print(Markdown(answer).data+"\n")
+					self.logger.add(question, answer)
+					self.logger.save()
+				except:
+					print("뭔가 비정상적인 일이 일어났는데, 뭔지는 모르겠네요. 처신하세요!")
 
-def main():
-	chat_bot = ChatBot(API_KEY, PDF_DOCUMENT)
-	prompt_creator = PromptCreator()
-	template = prompt_creator.create_template()
-	prompt = prompt_creator.from_template(template)
-	chain = chat_bot.create_chain(prompt)
-
-	history =[]
-
-	while len(history) < 10:
-		question =input("질문을 입력하세요(종료:'exit'): ")
-		if question.lower() == 'exit':
-			print("대화를 종료합니다")
-			return
-		history.append(question)
-		answer = chain.invoke({'question': question}).content
-		print(Markdown(answer).data)
-		
-		if len(history) ==10:
-			print("질문 횟수가 초과되어 대화를 종료합니다")
-			print("대화를 더 원하시는 경우 처음부터 재진행됩니다")
-			return
-	
-    
-	#print("질문과 답변 기록")
-	#for i, (q, a) in enumerate(zip(history, chain.invoke({'question': question}))):
-	#	pass
-		#print(f"{i+1}. 질문: {q}\n 답변: {a}\n")        
-
- 
 
 if __name__ == "__main__":
-	main()
+	print("실행중...")
+	main = Main(API_KEY, PDF_DOCUMENT)
+	main.run()
